fe88e12c10bd999ecb9f4acdd6555722ca828a3c
sudhanshu <sudhanshuraj8917@gmail.com>
2023-11-10 22:25:47 +0530

    first commit

---
+++ src/components/Mui-Button.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+const MuiButton = () => {
+    return (
+        <div>
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </div>
+    )
+}
+
+export default MuiButton
